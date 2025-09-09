cba78cea76e0c7508042b4a0abe2540e34ac67a9
Alec1999 <a.ours1999@gmail.com>
2025-09-09 16:01:32 -0700

    Add functions for basic math operators.

---
+++ main.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return (num1 + num2);
+};
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+};
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+};
+
+function divide(num1, num2) {
+    return (num1 / num2);
+};
+
+console.log(("Add 3 + 4: "), add(3, 4));
+console.log(("Subtract 9 - 4: "), subtract(9, 4));
+console.log(("Multiply 3 * 4: "), multiply(3, 4));
+console.log(("divide 15 / 3: "), divide(15, 3));
